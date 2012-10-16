fc4ee536df39e4dea6a022fe64dde8382276d2b0
Shannon Rush <shannonmrush@gmail.com>
2012-10-16 14:43:09 -0600

    first commit, skeleton android app

---
+++ src/com/rushdevo/camcam/CamCamActivity.java
@@ -0,0 +1,20 @@
+package com.rushdevo.camcam;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CamCamActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_cam);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_cam_cam, menu);
+        return true;
+    }
+}
