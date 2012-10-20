17718b5cc472fb609683c7cdc211f50d9b99fc5e
Shannon Rush <shannonmrush@gmail.com>
2012-10-19 18:44:26 -0600

    removing broadcast button and method, adding wifi direct setup and wifi direct broadcast receiver

--- src/com/rushdevo/camcam/CamCamActivity.java
+++ src/com/rushdevo/camcam/CamCamActivity.java
@@ -1,16 +1,38 @@
 package com.rushdevo.camcam;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
+
 public class CamCamActivity extends Activity {
+	
+	WifiP2pManager mManager;
+	Channel mChannel;
+	BroadcastReceiver mReceiver;
+	
+	IntentFilter mIntentFilter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam_cam);
+        
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
     }
 
     @Override
@@ -19,7 +41,16 @@ public class CamCamActivity extends Activity {
         return true;
     }
     
-    public void broadcast(View view) {
-    	
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
     }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }    
 }
---
+++ src/com/rushdevo/camcam/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,52 @@
+package com.rushdevo.camcam;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+
+	private WifiP2pManager manager;
+    private Channel channel;
+    private CamCamActivity activity;
+    
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, CamCamActivity activity) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+        	
+        	// Check to see if Wi-Fi is enabled and notify appropriate activity
+        	int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+        	if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi Direct is enabled
+            } else {
+                // Wi-Fi Direct is not enabled
+            }
+        
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+        	
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+        
+        	// Respond to new connection or disconnections
+        
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+        
+        	// Respond to this device's wifi state changing
+        
+        }
+    }
+    
+    
+}
